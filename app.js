371d412dcb70f0fe90c19dba8ebe8a993bb60422
Himitsu Fushigi <himitsu.fushigi@protonmail.com>
2021-10-21 23:35:56 +0545

    Initial Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+
+// @dev additional package setup here
+
+// @dev database connection setup
+
+// @dev routes setup here
+
+module.exports = app;
